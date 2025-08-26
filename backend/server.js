354f3291218f2058b52172d28464fb5e45be6559
parvinbiti <parvinbiti365@gmail.com>
2025-08-26 10:38:39 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = require("./Router/router");
 const app = express();
 const path = require("path");
-const port = 3000;
+const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 
 // Serve static files from the "public" directory
