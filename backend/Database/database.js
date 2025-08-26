02cdefba2ac29282770f6dfb0b69eb9397b40e8b
parvinbiti <parvinbiti365@gmail.com>
2025-08-26 12:27:29 -0400

    Update database.js

--- backend/Database/database.js
+++ backend/Database/database.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_user}:${process.env.DB_password}@cluster0.f9aqief.mongodb.net/${process.env.DB_name}?retryWrites=true&w=majority`,
+    // `mongodb+srv://${process.env.DB_user}:${process.env.DB_password}@cluster0.f9aqief.mongodb.net/${process.env.DB_name}?retryWrites=true&w=majority`,
+
+
+
+    mongodb+srv://sani:Hh1287@youtube.7upqego.mongodb.net/?retryWrites=true&w=majority&appName=youtube,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
