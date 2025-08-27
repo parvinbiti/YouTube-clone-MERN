b11628d9584fa73330668498c3a13bbe099a0d66
parvinbiti <parvinbiti365@gmail.com>
2025-08-27 06:12:39 -0400

    Update WatchLater.jsx

--- frontend/src/Components/WatchLater.jsx
+++ frontend/src/Components/WatchLater.jsx
@@ -9,9 +9,9 @@ import "react-loading-skeleton/dist/skeleton.css";
 import { useSelector } from "react-redux";
 
 function WatchLater() {
-  // const backendURL = "https://youtube-clone-mern-backend.vercel.app";
+  const backendURL = "https://youtube-clone-mern-2backend.onrender.com";
   // const backendURL = "http://localhost:3000";
-  const backendURL = process.env.REACT_APP_API_URL;
+  // const backendURL = process.env.REACT_APP_API_URL;
 
   const [email, setEmail] = useState();
   const [name, setName] = useState();
