841cc366cc869c0faeb3677f209e1e5477fbbbe3
parvinbiti <parvinbiti365@gmail.com>
2025-08-27 05:48:12 -0400

    Update LeftPanel.jsx

--- frontend/src/Components/LeftPanel.jsx
+++ frontend/src/Components/LeftPanel.jsx
@@ -37,8 +37,10 @@ import { MdVideoLibrary } from "react-icons/md";
 import { useSelector } from "react-redux";
 
 function LeftPanel() {
-  const backendURL = "https://youtube-clone-mern-backend.vercel.app"
+  // const backendURL = "https://youtube-clone-mern-backend.vercel.app"
   // const backendURL = "http://localhost:3000";
+  const backendURL = process.env.REACT_APP_API_URL;
+
   const [menuClicked, setMenuClicked] = useState(() => {
     const menu = localStorage.getItem("menuClicked");
     return menu ? JSON.parse(menu) : false;
