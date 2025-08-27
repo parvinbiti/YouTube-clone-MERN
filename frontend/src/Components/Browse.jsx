51f737205e042ff6a4c58f1c237989dda6ec04f0
parvinbiti <parvinbiti365@gmail.com>
2025-08-27 06:24:17 -0400

    Update Browse.jsx

--- frontend/src/Components/Browse.jsx
+++ frontend/src/Components/Browse.jsx
@@ -11,8 +11,8 @@ import "../Css/theme.css";
 import { useSelector } from "react-redux";
 
 function Browse() {
-  // const backendURL = "https://youtube-clone-mern-backend.vercel.app"
-  const backendURL = process.env.REACT_APP_API_URL;
+  const backendURL = "https://youtube-clone-mern-2backend.onrender.com"
+  // const backendURL = process.env.REACT_APP_API_URL;
 
   // const backendURL = "http://localhost:3000";
   const [thumbnails, setThumbnails] = useState([]);
