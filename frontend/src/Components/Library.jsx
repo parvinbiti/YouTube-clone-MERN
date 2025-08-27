db75ff644ea88c5d9e3183c74933f2bae7003222
parvinbiti <parvinbiti365@gmail.com>
2025-08-27 05:49:15 -0400

    Update Library.jsx

--- frontend/src/Components/Library.jsx
+++ frontend/src/Components/Library.jsx
@@ -30,7 +30,9 @@ function generateRandomColors(count) {
 }
 
 function Library() {
-  const backendURL = "https://youtube-clone-mern-backend.vercel.app"
+  // const backendURL = "https://youtube-clone-mern-backend.vercel.app"
+  const backendURL = process.env.REACT_APP_API_URL;
+
   // const backendURL = "http://localhost:3000";
   const [watchlater, setWatchLater] = useState([]);
   const [PlaylistData, setPlaylistData] = useState([]);
