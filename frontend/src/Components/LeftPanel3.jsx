e35677adabb93fe91f39afef8055b80d632d9ddb
parvinbiti <parvinbiti365@gmail.com>
2025-08-27 06:21:58 -0400

    Update LeftPanel3.jsx

--- frontend/src/Components/LeftPanel3.jsx
+++ frontend/src/Components/LeftPanel3.jsx
@@ -11,7 +11,7 @@ import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import { useSelector } from "react-redux";
 function LeftPanel2() {
-  // const backendURL = "https://youtube-clone-mern-backend.vercel.app"
+  const backendURL = "https://youtube-clone-mern-2backend.onrender.com"
   // const backendURL = "http://localhost:3000";
   const backendURL = process.env.REACT_APP_API_URL;
 
