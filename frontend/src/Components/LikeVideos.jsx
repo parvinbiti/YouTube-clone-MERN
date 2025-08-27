69bca1644546debb477bc5d22770cc26e1c9adb3
parvinbiti <parvinbiti365@gmail.com>
2025-08-27 05:49:36 -0400

    Update LikeVideos.jsx

--- frontend/src/Components/LikeVideos.jsx
+++ frontend/src/Components/LikeVideos.jsx
@@ -8,7 +8,9 @@ import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import { useSelector } from "react-redux";
 function LikeVideos() {
-  const backendURL = "https://youtube-clone-mern-backend.vercel.app";
+  // const backendURL = "https://youtube-clone-mern-backend.vercel.app";
+  const backendURL = process.env.REACT_APP_API_URL;
+
   // const backendURL = "http://localhost:3000";
   const [menuClicked, setMenuClicked] = useState(() => {
     const menu = localStorage.getItem("menuClicked");
