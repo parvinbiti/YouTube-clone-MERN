dfa98711dc4d53a431de3ec8f1494d82078b44c8
parvinbiti <parvinbiti365@gmail.com>
2025-08-27 06:27:15 -0400

    Update ChannelHome.jsx

--- frontend/src/Components/Channel/ChannelHome.jsx
+++ frontend/src/Components/Channel/ChannelHome.jsx
@@ -11,7 +11,7 @@ import "react-loading-skeleton/dist/skeleton.css";
 import { useSelector } from "react-redux";
 
 function ChannelHome(prop) {
-  const backendURL = "https://youtube-clone-mern-backend.vercel.app"
+  const backendURL = "https://youtube-clone-mern-2backend.onrender.com"
   // const backendURL = "http://localhost:3000"
   const [myVideos, setMyVideos] = useState([]);
   const [loading, setLoading] = useState(true);
