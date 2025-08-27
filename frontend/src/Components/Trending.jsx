fc6c1d56ef7922b93d844017973b77ee533d0c90
parvinbiti <parvinbiti365@gmail.com>
2025-08-27 06:15:03 -0400

    Update Trending.jsx

--- frontend/src/Components/Trending.jsx
+++ frontend/src/Components/Trending.jsx
@@ -12,9 +12,9 @@ import "react-loading-skeleton/dist/skeleton.css";
 import { useSelector } from "react-redux";
 
 function Trending() {
-  // const backendURL = "https://youtube-clone-mern-backend.vercel.app";
+  const backendURL = "https://youtube-clone-mern-2backend.onrender.com";
   // const backendURL = "http://localhost:3000";
-  const backendURL = process.env.REACT_APP_API_URL;
+  // const backendURL = process.env.REACT_APP_API_URL;
 
   const [trendingVideos, setTrendingVideos] = useState([]);
   const [menuClicked, setMenuClicked] = useState(() => {
