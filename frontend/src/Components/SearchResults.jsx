938852f23f93aef8a031015b24bc4a83abd7ae18
parvinbiti <parvinbiti365@gmail.com>
2025-08-27 06:17:59 -0400

    Update SearchResults.jsx

--- frontend/src/Components/SearchResults.jsx
+++ frontend/src/Components/SearchResults.jsx
@@ -17,9 +17,9 @@ import "react-toastify/dist/ReactToastify.css";
 import { useSelector } from "react-redux";
 
 function SearchResults() {
-  // const backendURL = "https://youtube-clone-mern-backend.vercel.app"
+  const backendURL = "https://youtube-clone-mern-2backend.onrender.com"
   // const backendURL = "http://localhost:3000";
-  const backendURL = process.env.REACT_APP_API_URL;
+  // const backendURL = process.env.REACT_APP_API_URL;
 
   const { data } = useParams();
   const [searchedVideoData, setsearchedVideoData] = useState([]);
