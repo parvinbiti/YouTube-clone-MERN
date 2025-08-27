9d2a2ceeb33fa52e8249af6329c7d252891dcc2f
parvinbiti <parvinbiti365@gmail.com>
2025-08-27 06:28:21 -0400

    Update FeaturedChannels.jsx

--- frontend/src/Components/Channel/FeaturedChannels.jsx
+++ frontend/src/Components/Channel/FeaturedChannels.jsx
@@ -10,7 +10,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { useSelector } from "react-redux";
 
 function FeaturedChannels(prop) {
-  const backendURL = "https://youtube-clone-mern-backend.vercel.app"
+  const backendURL = "https://youtube-clone-mern-2backend.onrender.com"
   // const backendURL = "http://localhost:3000";
   const [addChannelClicked, setAddChannelClicked] = useState(false);
   const [Subscriptions, setSubscriptions] = useState([]);
